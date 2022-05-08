6b1b689e41412fdf291b36d18db28855f66d55b0
yoshikawa(Taiki Yoshikawa) <yoshikawataiki@gmail.com>
2022-05-08 16:15:39 +0900

    add: db.rs
    データベースへのコネクションプールの作成

---
+++ server/src/db.rs
@@ -0,0 +1,18 @@
+use std::env;
+
+use diesel::{
+    r2d2::{self, ConnectionManager},
+    PgConnection,
+};
+
+pub type Pool = r2d2::Pool<ConnectionManager<PgConnection>>;
+
+pub fn create_db_connection_pool() -> Pool {
+    let database_url = env::var("DATABASE_URL").expect("DATABASE_URL must be set.");
+    //create connection manager for pool
+    let manager = ConnectionManager::<PgConnection>::new(database_url);
+    //connection pool
+    r2d2::Pool::builder()
+        .build(manager)
+        .expect("failed to create pool")
+}
--- server/src/main.rs
+++ server/src/main.rs
@@ -1,8 +1,8 @@
+use actix_redis::RedisActor;
+use actix_web::{web, App, HttpResponse, HttpServer, Responder, Result};
 use std::env;
 
-use actix_files;
-use actix_web::{web, App, HttpResponse, HttpServer, Responder, Result};
-type RedisConn = r2d2_redis::r2d2::PooledConnection<r2d2_redis::RedisConnectionManager>;
+mod db;
 
 async fn index() -> Result<actix_files::NamedFile> {
     Ok(actix_files::NamedFile::open("build/index.html")?)
@@ -16,18 +16,18 @@ async fn manual_hello() -> impl Responder {
 async fn main() -> std::io::Result<()> {
     let host = env::var("HOST").unwrap_or_else(|_| "0.0.0.0".to_string());
     let port = env::var("PORT").unwrap_or_else(|_| "8000".to_string());
-    let psql_url = env::var("DATABASE_URL").unwrap_or_else(|_| "postgres://mendan:password@db/mendan".to_string());
+    let conn_db_pool = db::create_db_connection_pool();
     let redis_host = env::var("REDIS_HOST").unwrap_or_else(|_| "redis".to_string());
     let redis_port = env::var("REDIS_HOST").unwrap_or_else(|_| "6379".to_string());
 
-    HttpServer::new(|| {
+    HttpServer::new(move || {
         App::new()
+            .app_data(conn_db_pool.clone())
+            .app_data(RedisActor::start(format!("{}:{}", redis_host, redis_port)))
             .route("/", web::get().to(index))
             .route("/hey", web::get().to(manual_hello))
             .service(actix_files::Files::new("", "build"))
-            .default_service(
-                web::route().to(index)
-            )
+            .default_service(web::route().to(index))
     })
     .bind(format!("{}:{}", host, port))?
     .run()
