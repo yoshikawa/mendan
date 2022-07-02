dcc35110fe9e41267dd82ca713fca304c8891a12
yoshikawa(Taiki Yoshikawa) <yoshikawataiki@gmail.com>
2022-07-02 23:07:20 +0900

    [fix] diesel->SeaORM

---
+++ server/migration/src/lib.rs
@@ -0,0 +1,16 @@
+pub use sea_orm_migration::prelude::*;
+
+mod m20220702_000001_create_user;
+mod m20220702_000002_create_reservation;
+mod m20220702_000003_create_pre_user;
+
+pub struct Migrator;
+
+#[async_trait::async_trait]
+impl MigratorTrait for Migrator {
+    fn migrations() -> Vec<Box<dyn MigrationTrait>> {
+        vec![Box::new(m20220702_000001_create_user::Migration)]
+        vec![Box::new(m20220702_000002_create_reservation::Migration)]
+        vec![Box::new(m20220702_000003_create_pre_user::Migration)]
+    }
+}
---
+++ server/migration/src/m20220702_000001_create_user.rs
@@ -0,0 +1,20 @@
+use sea_orm_migration::prelude::*;
+
+pub struct Migration;
+
+impl MigrationName for Migration {
+    fn name(&self) -> &str {
+        "m20220702_000001_create_user"
+    }
+}
+
+#[async_trait::async_trait]
+impl MigrationTrait for Migration {
+    async fn up(&self, manager: &SchemaManager) -> Result<(), DbErr> {
+        todo!()
+    }
+
+    async fn down(&self, manager: &SchemaManager) -> Result<(), DbErr> {
+        todo!()
+    }
+}
